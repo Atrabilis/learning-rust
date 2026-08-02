c3e8a160e3010358b51c1491d6c81195a8480321
Angel Andrade <angel.bruno.andrade.osorio@gmail.com>
2026-08-02 13:22:37 -0400

    moved exercises

---
+++ exercises/find_first_index/find_first_index.rs
@@ -0,0 +1,21 @@
+fn find_first_index(numbers: &[i32], target: i32) -> Option<usize> {
+    let mut index: usize = 0;
+    for number in numbers {
+        if *number == target {
+            return Some(index);
+        }
+        index += 1;
+    }
+    None
+}
+
+fn main() {
+    if let Some(index) = find_first_index(&[4, 8, 2, 8], 8) {
+        println! {"{}",index}
+    }
+    if let Some(index) = find_first_index(&[4, 8, 2, 8], 5) {
+        println! {"{}",index};
+    } else {
+        println!("None");
+    }
+}
